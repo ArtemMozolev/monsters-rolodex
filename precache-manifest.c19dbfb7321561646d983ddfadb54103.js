02ce5e4e7d128e6ae3b12d6616d27e7f1fb81939
Артем Мозолев <artemmozolev@MacBook-Pro-Artem-3.local>
2020-04-19 23:55:21 +0300

    Updates

---
+++ precache-manifest.c19dbfb7321561646d983ddfadb54103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425316450254725618e20d203a25c074",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "070330449c3dfc60de9b",
+    "url": "/monsters-rolodex/static/css/main.b694bff5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "070330449c3dfc60de9b",
+    "url": "/monsters-rolodex/static/js/main.837855d7.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
